6860aa49bb26779247a696a7674539de17790fed
cealach <cealach.oconnor@cattleeye.com>
2023-07-31 16:41:11 +0100

    chore: game header

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './Styles/Game.css'
 
 const Game = () => {
   return (
     <div>
-
+      <h1>Whack A Mole!</h1>
+      <button className="start" >Start Game</button>
     </div>
   )
 }
